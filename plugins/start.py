31775ee8a39f3f981d40b6aa419c62af83ff4cd8
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-14 19:20:39 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,12 +1,26 @@
-#(©)Codexbotz
 import os
 import asyncio
 from pyrogram import Client, filters, __version__
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
-
+from pyrogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
+)
+from pyrogram.errors import (
+    FloodWait,
+    UserIsBlocked,
+    InputUserDeactivated,
+)
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
+from config import (
+    ADMINS,
+    FORCE_MSG,
+    START_MSG,
+    OWNER_ID,
+    CUSTOM_CAPTION,
+    DISABLE_CHANNEL_BUTTON,
+)
 from helper_func import subscribed, encode, decode, get_messages
 from database.support import users_info
 from database.sql import add_user, query_msg
