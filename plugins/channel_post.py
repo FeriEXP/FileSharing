9cdb25c301abda41313c11ad0eaf417c22f83018
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-14 19:32:07 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,6 +1,10 @@
 import asyncio
 from pyrogram import filters, Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 from pyrogram.errors import FloodWait
 from bot import Bot
 from config import (
