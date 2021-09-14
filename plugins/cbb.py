ec8844401edb8f69e925fbefc1ed77e0a101cac2
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-14 19:30:46 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,9 +1,13 @@
-#(©)Codexbotz
-
+from pyrogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
+)
 from pyrogram import __version__
-from bot import Bot
 from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from bot import Bot
+
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
